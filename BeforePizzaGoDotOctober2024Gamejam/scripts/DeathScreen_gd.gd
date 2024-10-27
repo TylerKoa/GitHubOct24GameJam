b250d40cb10b18df95be5ec474ab42b5c49e652a
RedRex24 <drschweiger24@gmail.com>
2024-10-27 12:16:46 -0600

    Death and Main menu Screen Update

--- BeforePizzaGoDotOctober2024Gamejam/scripts/DeathScreen_gd.gd
+++ BeforePizzaGoDotOctober2024Gamejam/scripts/DeathScreen_gd.gd
@@ -1,12 +1,13 @@
-class_name MainMenu
+
 extends Control
 @onready var exit_game: Button = $TextureRect/ExitGame
 @onready var start_game: Button = $TextureRect/StartGame
-@onready var start_level = preload("res://scenes/game.tscn") as PackedScene
+@onready var start_level = preload("res://scenes/main_menu.tscn") as PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	$TextureRect/AnimatedSprite2D.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,7 +18,3 @@ func _process(delta: float) -> void:
 
 func _on_start_game_button_down() -> void:
 	get_tree().change_scene_to_packed(start_level)
-
-
-func _on_exit_game_button_down() -> void:
-	get_tree().quit()
